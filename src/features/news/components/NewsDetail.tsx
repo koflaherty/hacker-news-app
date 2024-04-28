b825495af33dd8951060bd84729bd517e78c3ed2
Kevin O'Flaherty <koflaherty@ymail.com>
2024-04-28 14:18:34 -0400

    Added toggling stars in redux state

--- src/features/news/components/NewsDetail.tsx
+++ src/features/news/components/NewsDetail.tsx
@@ -1,6 +1,22 @@
 import {hackerNewsFirebaseApi} from "../api/hacker-news-firebase-api.ts";
+import {toggleStared} from "../newsSlice.ts";
+import {useAppDispatch, useAppSelector} from "../../../store/hooks.ts";
 
 export const NewsDetail = ({id}: {id: string}) => {
+  const dispatch = useAppDispatch();
+  const isStarred = Boolean(useAppSelector((state) => state.news.stared[id]));
   const results = hackerNewsFirebaseApi.useGetDetailsQuery({id});
-  return <div>{JSON.stringify(results)}</div>;
+
+  return (
+    <div>
+      {JSON.stringify(results)}{" "}
+      <button
+        onClick={() => {
+          dispatch(toggleStared(id));
+        }}
+      >
+        {isStarred ? "un-star" : "star"}
+      </button>
+    </div>
+  );
 };
--- src/features/news/newsSlice.ts
+++ src/features/news/newsSlice.ts
@@ -1,17 +1,27 @@
-import {createSlice} from "@reduxjs/toolkit";
+import {createSlice, PayloadAction} from "@reduxjs/toolkit";
 
 export interface NewsState {
-  latest: string[];
+  stared: Record<string, true>;
 }
 
 const initialState: NewsState = {
-  latest: [],
+  stared: {},
 };
 
 const newsSlice = createSlice({
   name: "news",
   initialState: initialState,
-  reducers: {},
+  reducers: {
+    toggleStared: (state, action: PayloadAction<string>) => {
+      const id = action.payload;
+      if (state.stared[id]) {
+        delete state.stared[id];
+      } else {
+        state.stared[id] = true;
+      }
+    },
+  },
 });
 
+export const {toggleStared} = newsSlice.actions;
 export const newsReducer = newsSlice.reducer;
